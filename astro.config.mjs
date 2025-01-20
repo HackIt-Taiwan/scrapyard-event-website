1b5d3e66afd7cd1dc96a2d09a8c3a7839048ec3c
wolf-yuan <wolf@wolf-yuan.dev>
2025-01-21 00:03:03 +0800

    feat: language switcher

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 export default defineConfig({
-  redirects: {
-    "/": "/en/",
-  },
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
 });
--- src/i18n/locales/en.ts
+++ src/i18n/locales/en.ts
@@ -1,7 +1,7 @@
 import type { Locale } from "../index";
 
 export default {
-  locale: "en",
+  locale: "us",
   strings: {
     "home.title": "Build stupid S#!T, get stupid prizes!",
     "home.subtitle": "Coming soon at Taiwan XX/XX 12:00 PN",
