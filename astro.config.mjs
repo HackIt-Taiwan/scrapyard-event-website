1a83f52111059364705971cf793155a3ce6886b8
wolf-yuan <wolf@wolf-yuan.dev>
2025-02-01 13:56:10 +0800

    refactor: migrate to tailwindcss v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [icon()],
+  vite: { plugins: [tailwindcss()] },
 });
