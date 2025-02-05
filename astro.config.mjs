9242f8bb06aa5e4c31a503025b90323b27b9410b
wolf-yuan <wolf@wolf-yuan.dev>
2025-02-05 23:43:04 +0800

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
-  integrations: [icon()],
+  site: "https://scrapyard.hackit.tw/",
+  integrations: [icon(), sitemap()],
   vite: { plugins: [tailwindcss()] },
 });
