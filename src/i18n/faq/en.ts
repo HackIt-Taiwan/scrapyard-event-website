b0f76ab8d018ee589a220bb07ad7f6e9f6e09d3b
wolf-yuan <wolf@wolf-yuan.dev>
2025-02-06 23:59:01 +0800

    chore: update faq english translations

--- src/i18n/faq/en.ts
+++ src/i18n/faq/en.ts
@@ -4,82 +4,84 @@ export default {
   locale: "us",
   faq: [
     {
-      title: "What about the food?",
+      title: "What about food?",
       answer:
-        "HackIt will provide sufficient meals, including breakfast, lunch, dinner, snacks, drinks, bread, and more. A food bar will also be set up for participants to help themselves, ensuring they can replenish energy when needed!",
+        "We will provide plenty of meals, including three meals, snacks, drinks, and bread. A food bar will also be available for participants to grab what they need to stay energized!",
     },
     {
       title: "What about accommodation?",
-      answer: "We will arrange accommodation! We will arrange accommodation so that you can have a wonderful sleep~.",
-    },
-    {
-      title: "Is there any transportation subsidy?",
       answer:
-        "Hack Club might offer subsidies! However, more details will be available when Hack Club releases further information~\n*Any updates will be published on the official website.",
+        "We’ve got you covered! Accommodation will be arranged so you can get a good night’s rest.",
     },
     {
-      title: "Do low-income participants still need to pay?",
-      answer: "We are still negotiating with sponsors! We will notify you as soon as possible if there is any progress!",
-    },
-    {
-      title: "Do we have to be present at the event?",
+      title: "Is there a transportation subsidy?",
       answer:
-        "For this event, participants will be creating unique projects with their teams on-site, so attendance is required!",
+        "Hack Club might provide subsidies! However, detailed information will be announced by Hack Club later. Updates will be posted on the official website.",
+    },
+    {
+      title: "Do low-income participants still have to pay?",
+      answer:
+        "We’re still negotiating with sponsors! Any updates will be shared on the official website as soon as possible.",
+    },
+    {
+      title: "Is it mandatory to attend in person?",
+      answer:
+        "Yes! This event requires participants to be on-site to collaborate with their teammates and create unique projects.",
     },
     {
       title: "When does registration start?",
       answer:
-        "The registration system will open on 2/11! The latest news will be posted on the official website.",
+        "The registration system will open on February 11. Stay tuned for the latest updates on our official website.",
     },
     {
-      title: "How will we know if we are selected?",
+      title: "How will I know if we’re selected?",
       answer:
-        "The list of selected participants will be announced on 3/5 on the official website. Please stay tuned to the official website for the latest updates!",
+        "The selected participants list will be announced on March 5 on the official website. Please keep an eye on the latest updates!",
     },
     {
-      title: "Can you be trusted?",
+      title: "Can we trust you?",
       answer:
-        "Although we are just a group of high school students, each of us has rich backgrounds and experience! We also collaborate with government agencies and Hack Club in the U.S., and we can be trusted just like Panadol!",
+        "Although we’re just high school students, we all have relevant experience and backgrounds. Plus, we collaborate with government organizations and Hack Club in the U.S., making us as trustworthy as Tylenol!",
     },
     {
       title: "Can I attend for just half a day?",
       answer:
-        "This event is overnight, and the schedule spans two days. You need to be present for the entire duration of the event!",
+        "This is an overnight event that spans two days, so participants must stay for the entire duration.",
     },
     {
-      title: "What is the theme?",
+      title: "What’s the hackathon topic?",
       answer:
-        "The event is designed to encourage creativity and experimentation. Participants will challenge themselves to create the most innovative, humorous, or unconventional projects. There are no restrictions on themes or topics!",
+        "There are no restrictions on topics! This event is all about encouraging creativity, experimentation, and building unique, funny, or innovative projects.",
     },
     {
       title: "Who can participate?",
       answer:
-        "Anyone who is currently in high school and interested in programming can join, regardless of experience. We welcome everyone to participate!",
+        "Any high school student interested in programming is welcome to join, regardless of experience!",
     },
     {
-      title: "How many people can join?",
+      title: "How many people can join as a team?",
       answer:
-        "The event is team-based, with each team consisting of 4 to 5 members. If you're interested in joining but don't have a team, feel free to join our official Discord group to find teammates!",
+        "Teams should consist of 4 to 5 members. If you don’t have a team, join our official Discord community to find teammates!",
     },
     {
-      title: "Is a teacher required?",
+      title: "Is having a mentor mandatory?",
       answer:
-        "While there will be staff on-site, to ensure the safety of participants, a teacher must be present for each team!",
+        "Yes, for participants' safety, having a mentor is required, even though staff will also be present on-site.",
     },
     {
       title: "Can parents attend?",
       answer:
-        "Due to venue limitations, we do not have designated areas for parents to rest. We apologize for this. However, if parents have concerns, they are welcome to reach out to us via email or contact us for further inquiries!",
+        "Due to space limitations, we haven’t arranged a dedicated resting area for parents. However, if parents have concerns, they are welcome to contact us via email.",
     },
     {
-      title: "Can teachers attend?",
+      title: "Can mentors attend?",
       answer:
-        "Currently, for safety reasons, teachers must be present for the entire event. We will also provide a designated rest area for teachers on-site.",
+        "Yes, mentors are required to stay throughout the event for participants’ safety. Rest areas will be provided for mentors on-site.",
     },
     {
-      title: "Is there adult supervision on-site?",
+      title: "Will there be adult supervision on-site?",
       answer:
-        "Although the HackIt team consists of high school students, we have adult members as well! There will also be teachers present to ensure supervision, so there's no need to worry!",
+        "Although the HackIt team consists of high school students, we also have adult team members, and mentors will be present throughout the event. No need to worry!",
     },
   ],
 } satisfies Faq;
