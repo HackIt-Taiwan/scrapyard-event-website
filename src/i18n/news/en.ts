edf31b1add997e85edcc07a4b0db41a8b30b3476
wolf-yuan <wolf@wolf-yuan.dev>
2025-03-15 09:49:18 +0800

    feat: add youtube livestream url

--- src/i18n/news/en.ts
+++ src/i18n/news/en.ts
@@ -4,7 +4,13 @@ export default {
   locale: "us",
   news: [
     {
-      title: "Scrapyard 2025 is open for registration now!",
+      title: "Scrapyard livestream is now online!",
+      date: "2025-03-15",
+      content:
+        "Scrapyard Taiwan livestream is now online! Head over to <a href='https://www.youtube.com/watch?v=X1O9DE6GDOY'>YouTube</a> to watch it!",
+    },
+    {
+      title: "Scrapyard is open for registration now!",
       date: "2025-02-10",
       content:
         "Scrapyard hackathon is open for registration now! Head over to our Discord server or send an email to our official mailbox if you have any question!",
--- src/i18n/news/tw.ts
+++ src/i18n/news/tw.ts
@@ -2,7 +2,12 @@ import type { News } from "../index.ts";
 
 export default {
   locale: "tw",
-  news: [
+  news: [{
+    title: "Scrapyard 黑客松的直播上線了!",
+    date: "2025-03-15",
+    content:
+      "歡迎至 <a href='https://www.youtube.com/watch?v=X1O9DE6GDOY'>YouTube</a> 來觀看直播！",
+  },
     {
       title: "Scrapyard 2025 已開始報名！",
       date: "2025-02-10",
